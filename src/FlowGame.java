8495bdfa09cd299a5a4f01fb8311697854c5ab5b
liuyuxuan123 <1010134430@qq.com>
2018-11-21 00:32:16 -0500

    Flow Game: Lookup Functions

--- src/FlowGame.java
+++ src/FlowGame.java
@@ -7,6 +7,27 @@
 //     5    	        ┐	        bottom-left
 //     6    	        ┌	        bottom-right
 
+
+//  Letter	Color
+//     R	 red
+//     B	 blue
+//     Y	 yellow
+//     G	 green
+//     O	 orange
+//     C	 cyan
+//     M	 magenta
+//     m	 maroon
+//     P	 purple
+//     A	 gray
+//     W	 white
+//     g	 bright green
+//     T	 tan
+//     b	 dark blue
+//     c	 dark cyan
+//     p	 pink
+
+
+
 //  Every cell is assigned a single color.
 //  The color of every endpoint cell is known and specified.
 //  Every endpoint cell has exactly one neighbor which matches its color.
@@ -17,7 +38,10 @@
 
 import java.util.HashMap;
 import java.io.File;
+import java.net.URL;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class FlowGame{
     static int LEFT =   1 << 0;
@@ -42,11 +66,27 @@ public class FlowGame{
         put(BOTTOM_RIGHT, "┌");
     }};
 
+    static HashMap<String, Integer> ANSI_LOOKUP = new HashMap<>() {{
+        put("R",101);   put("B",104);   put("Y",103);   put("G",42);
+        put("O",43);    put("C",106);   put("M",105);   put("m",41);
+        put("P",45);    put("A",100);   put("W",107);   put("g",102);
+        put("T",47);    put("b",44);    put("c",46);    put("p",35);
+    }};
+
 
     public void parsePuzzle(String filePosition){
         //InputStream input = getClass().getResourceAsStream("ListStopWords.txt");
         String relativeFilePosition = "puzzles/" + filePosition;
-        System.out.println(getClass().getResource(relativeFilePosition));
+        URL relativeFileURL = getClass().getResource(relativeFilePosition);
+        File gameFile = new File(relativeFileURL.getPath());
+        try (BufferedReader br = new BufferedReader(new FileReader(gameFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     static public void main(String[] args){
